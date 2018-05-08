ef66c0b3e9bd015c012ee2405a9ce7ba14f57a28
Michael Panunto <mpanunto@seradex.local>
2018-05-08 16:38:06 -0400

    File location and single page scanning working

---
+++ AutomatedScanner/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AutomatedScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this._twain = new Saraff.Twain.Twain32(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnDS = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(1);
+            this.pictureBox1.Size = new System.Drawing.Size(714, 630);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // _twain
+            // 
+            this._twain.AppProductName = "Saraff.Twain.NET";
+            this._twain.Parent = this;
+            this._twain.AcquireCompleted += new System.EventHandler(this._twain_AcquireCompleted);
+            this._twain.TwainStateChanged += new System.EventHandler<Saraff.Twain.Twain32.TwainStateEventArgs>(this._twain_TwainStateChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(734, 711);
+            this.tableLayoutPanel1.TabIndex = 3;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnSave);
+            this.flowLayoutPanel1.Controls.Add(this.btnDS);
+            this.flowLayoutPanel1.Controls.Add(this.btnScan);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(456, 658);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(275, 41);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // btnDS
+            // 
+            this.btnDS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(50)))), ((int)(((byte)(63)))));
+            this.btnDS.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDS.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDS.ForeColor = System.Drawing.Color.White;
+            this.btnDS.Location = new System.Drawing.Point(103, 3);
+            this.btnDS.Name = "btnDS";
+            this.btnDS.Size = new System.Drawing.Size(88, 35);
+            this.btnDS.TabIndex = 1;
+            this.btnDS.Text = "Select DS";
+            this.btnDS.UseVisualStyleBackColor = false;
+            this.btnDS.Click += new System.EventHandler(this.btnDS_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.AutoSize = true;
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(50)))), ((int)(((byte)(63)))));
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnSave.Location = new System.Drawing.Point(3, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 35);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Destination";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnScan
+            // 
+            this.btnScan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(50)))), ((int)(((byte)(63)))));
+            this.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnScan.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.ForeColor = System.Drawing.Color.White;
+            this.btnScan.Location = new System.Drawing.Point(197, 3);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 35);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = false;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // folderBrowser
+            // 
+            this.folderBrowser.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(206)))), ((int)(((byte)(199)))));
+            this.ClientSize = new System.Drawing.Size(734, 711);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Scan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Saraff.Twain.Twain32 _twain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        internal System.Windows.Forms.Button btnDS;
+    }
+}
+
---
+++ AutomatedScanner/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Saraff.Twain;
+using PdfSharp;
+
+namespace AutomatedScanner
+{
+    public partial class Form1 : Form
+    {
+        private const string _x86Aux = "Saraff.Twain.Aux_x86.exe"; // For twain 1.x
+        private const string _msilAux = "Saraff.Twain.Aux_MSIL.exe"; // For twain 2.x
+        private string destination = "./"; // Default destination
+        private bool _isEnable = false;
+
+        /// <summary>
+        /// Constructs the form window and opens the twain data source manager
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                this._twain.OpenDSM();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("{0}\n\n{1}", ex.Message, ex.StackTrace), "Scanner", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Lists all available data sources available and allows the user to select device
+        /// </summary>
+        /// <param name="sender">Provides context regarding the object that raised the event</param>
+        /// <param name="e">Contains the data pertaining to the event</param>
+        private void btnDS_Click(object sender, EventArgs e)
+        {
+            this._twain.CloseDataSource();
+            this._twain.SelectSource();
+        }
+
+        /// <summary>
+        /// Initializes the scan using the selected data source
+        /// </summary>
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this._twain.Acquire();
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Scanner", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Opens a folder browser and overrides the default destination with the selected path
+        /// </summary>
+        /// <param name="sender">Provides context regarding the object that raised the event</param>
+        /// <param name="e">Contains the data pertaining to the event</param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                destination = folderBrowser.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// Actions to occur after a completed scan; previews the scan and saves it as a pdf
+        /// </summary>
+        /// <param name="sender">Provides context regarding the object that raised the event</param>
+        /// <param name="e">Contains the data pertaining to the event</param>
+        private void _twain_AcquireCompleted(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.pictureBox1.Image != null)
+                {
+                    this.pictureBox1.Dispose();
+                }
+                if (this._twain.ImageCount > 0)
+                {
+                    this.pictureBox1.Image = this._twain.GetImage(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Scanner", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            SaveScan(destination, this._twain.GetImage(0));
+        }
+            
+        /// <summary>
+        /// Method for dealing with a twain state change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _twain_TwainStateChanged(object sender, Twain32.TwainStateEventArgs e)
+        {
+            try
+            {
+
+                if ((e.TwainState & Twain32.TwainStateFlag.DSEnabled) == 0 && this._isEnable)
+                {
+                    this._isEnable = false;
+                }
+                this._isEnable = (e.TwainState & Twain32.TwainStateFlag.DSEnabled) != 0;
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Scanner", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Saves the returned image as a pdf
+        /// </summary>
+        /// <param name="destination">Location where the pdf will be saved</param>
+        /// <param name="originalImage">Image returned from the scan</param>
+        private void SaveScan(string destination, System.Drawing.Image originalImage)
+        {
+            PdfSharp.Pdf.PdfDocument document = new PdfSharp.Pdf.PdfDocument();
+            PdfSharp.Pdf.PdfPage page = document.AddPage();
+            PdfSharp.Drawing.XGraphics xgfx = PdfSharp.Drawing.XGraphics.FromPdfPage(page);
+            PdfSharp.Drawing.XImage ximg = PdfSharp.Drawing.XImage.FromGdiPlusImage(originalImage);
+
+
+            xgfx.DrawImage(ximg, 0, 0);
+            string filename = "testScan.pdf";
+            document.Save(destination + "/" + filename);
+            document.Close();
+            Console.WriteLine(Environment.CurrentDirectory);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
